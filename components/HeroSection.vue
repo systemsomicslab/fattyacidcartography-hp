fa8147732f9d59913faebee12580b3041e6f97f2
reika0717 <reikahirahara@gmail.com>
2025-06-11 17:06:43 +0900

    タイトル周りのshadow調整

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,8 +1,14 @@
 <template>
   <section
     class="relative bg-cover bg-center bg-no-repeat min-h-[400px] flex flex-col items-center justify-center text-center text-white px-4"
-    :style="{ backgroundImage: `url('/images/hero-background.png')` }">
-    <h1 class="text-2xl sm:text-4xl md:text-4xl font-bold mb-2 sm:mb-4 drop-shadow-lg">
+    :style="{
+      backgroundImage: `url('/images/hero-background.png')`,
+      boxShadow: 'inset 0 0 9px 4px rgba(0,0,0,0.4)'
+    }">
+    <h1
+      class="text-2xl sm:text-4xl md:text-4xl font-bold mb-2 sm:mb-4 drop-shadow-lg"
+      :style="{ textShadow: '0 0 6px rgba(0, 0, 0, 0.4)' }"
+    >
       {{ $t('hero.title') }}
     </h1>
     <p class="text-sm sm:text-base md:text-lg font-medium bg-black bg-opacity-70 px-4 py-2">
--- components/SubPageHeroSection.vue
+++ components/SubPageHeroSection.vue
@@ -6,10 +6,16 @@ const props = defineProps<{
 </script>
 <template>
   <section class="relative bg-cover bg-center bg-no-repeat min-h-[200px] flex flex-col justify-center text-white"
-    :style="{ backgroundImage: `url('/images/hero-background.png')` }">
-    <div class="absolute inset-0 bg-black/30"></div>
+    :style="{
+      backgroundImage: `url('/images/hero-background.png')`,
+      boxShadow: 'inset 0 0 9px 4px rgba(0,0,0,0.4)'
+    }"
+  >
+    <div class="absolute inset-0 bg-black/10"></div>
     <h1
-      class="text-2xl sm:text-4xl md:text-4xl font-bold mb-2 sm:mb-4 drop-shadow-lg max-w-6xl px-4 md:px-14 mx-auto w-full">
+      class="text-2xl sm:text-4xl md:text-4xl font-bold mb-2 sm:mb-4 drop-shadow-lg max-w-6xl px-4 md:px-14 mx-auto w-full"
+      :style="{ textShadow: '0 0 6px rgba(0, 0, 0, 0.4)' }"
+    >
       {{ props.title }}
     </h1>
   </section>
