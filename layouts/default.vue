7c8158b556367d9f5076d6613c880a01d1f3b775
reika0717 <reikahirahara@gmail.com>
2025-06-12 12:09:31 +0900

    SPのマージン設定できないバグ修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { HEADER_HEIGHT_PC } from '~/components/Header.vue'
-const isSP = computed(() => {
-    if (process.client) {
-      return window.innerWidth < 768;
-    }
-    return false;
+const isSP = ref(false)
+
+onMounted(() => {
+    isSP.value = window.innerWidth < 768;
+    window.addEventListener('resize', () => {
+        isSP.value = window.innerWidth < 768;
+    });
 });
 </script>
 
