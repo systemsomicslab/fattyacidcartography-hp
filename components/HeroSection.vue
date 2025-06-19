c0ee92f771de43de5b0f13c33c3681e32d9d46b0
reika0717 <reikahirahara@gmail.com>
2025-06-19 13:12:57 +0900

    メインビジュアルデザイン修正

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,18 +1,23 @@
 <template>
   <section
-    class="relative bg-cover bg-center bg-no-repeat min-h-[400px] flex flex-col items-center justify-center text-center text-white px-4"
+    class="relative bg-cover bg-center bg-no-repeat min-h-[400px] flex flex-col items-center justify-center text-center text-white"
     :style="{
       backgroundImage: `url('${useRuntimeConfig().app.baseURL}images/eyecatch.jpg')`,
-      boxShadow: 'inset 0 0 9px 4px rgba(0,0,0,0.3)'
+      boxShadow: 'inset 0 0 9px 5px rgba(0,0,0,0.1)'
     }">
-    <h1
-      class="text-2xl sm:text-4xl md:text-4xl font-bold mb-2 sm:mb-4 drop-shadow-lg"
-      :style="{ textShadow: '0 0 6px rgba(0, 0, 0, 0.4)' }"
-    >
-      {{ $t('hero.title') }}
-    </h1>
-    <p class="text-sm sm:text-base md:text-lg font-medium bg-black bg-opacity-70 px-4 py-2">
-      {{ $t('hero.subtitle') }}
-    </p>
+    <div class="bg-gradient-to-r from-primary-light-blue-hero-bg to-primary-light-green-hero-bg px-8 py-4">
+      <h1
+        class="text-2xl sm:text-4xl md:text-4xl font-bold mb-2 drop-shadow-lg"
+        :style="{ textShadow: '0 0 6px rgba(0, 0, 0, 0.3)' }"
+      >
+        {{ $t('hero.title') }}
+      </h1>
+      <p
+        class="text-sm sm:text-base md:text-lg font-medium"
+        :style="{ textShadow: '0 0 6px rgba(0, 0, 0, 0.3)' }"
+      >
+        {{ $t('hero.subtitle') }}
+      </p>
+    </div>
   </section>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,10 @@ export default {
       colors: {
         'link-color': '#0B5FA5',
         'primary-light-blue': '#4ECEEF',
+        'primary-light-blue-hero-bg': 'rgba(36, 195, 235, 0.6)',
         'primary-light-blue-opacity': 'rgba(78, 206, 239, 0.2)',
         'primary-light-green': '#78CC38',
+        'primary-light-green-hero-bg': 'rgba(106, 182, 47, 0.6)',
         'primary-light-green-opacity': 'rgba(120, 204, 56, 0.2)',
       },
     }
